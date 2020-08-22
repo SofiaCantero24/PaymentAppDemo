7ceeea9008936686de545c202d34f5398e59502a
SofiaCantero24 <canterosofia24@gmail.com>
2020-08-22 18:21:48 -0300

    project created

---
+++ PaymentsAppDemo/PaymentsAppDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PaymentsAppDemo
+//
+//  Created by Laura Sofia Cantero Hernandez on 8/22/20.
+//  Copyright © 2020 Sofia Cantero. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
